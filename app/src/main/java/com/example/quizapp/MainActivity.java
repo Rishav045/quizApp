8a30f3fa275e29bcad6507bdbae441f21a9fa9b9
Rishav Kumar <20bds045@iiitdwd.ac.in>
2022-10-11 18:55:01 +0530

    first commit

---
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView totalQuestions , questionTextView;
+    Button ansA, ansB, ansC, ansD,submitBtn;
+
+    int score =0;
+    int totalQuestion = QuestionAnswer.question.length;
+    int currentQuestionIndex=0;
+    String selectedAnswer ="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        totalQuestions= findViewById(R.id.textView1);
+        questionTextView= findViewById(R.id.textView2);
+        ansA = findViewById(R.id.AnsA);
+        ansB = findViewById(R.id.AnsB);
+        ansC = findViewById(R.id.AnsC);
+        ansD = findViewById(R.id.AnsD);
+        submitBtn = findViewById(R.id.submit);
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+        totalQuestions.setText("Total questions :"+totalQuestion);
+
+        loadNewQuestion();
+
+    }
+    @Override
+    public void onClick(View view) {
+
+        ansA.setBackgroundColor(Color.WHITE);
+        ansB.setBackgroundColor(Color.WHITE);
+        ansC.setBackgroundColor(Color.WHITE);
+        ansD.setBackgroundColor(Color.WHITE);
+
+        Button clickedButton = (Button) view;
+        if(clickedButton.getId()==R.id.submit){
+            if(selectedAnswer.equals(QuestionAnswer.correctAnswer[currentQuestionIndex])){
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+
+
+        }else{
+            //choices button clicked
+            selectedAnswer  = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.MAGENTA);
+
+        }
+
+    }
+
+    void loadNewQuestion(){
+
+        if(currentQuestionIndex == totalQuestion ){
+            finishQuiz();
+            return;
+        }
+
+        questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
+        ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
+        ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
+        ansC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
+        ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
+
+    }
+
+    void finishQuiz(){
+        String passStatus = "";
+        if(score > totalQuestion*0.60){
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score is "+ score+" out of "+ totalQuestion)
+                .setPositiveButton("Restart",(dialogInterface, i) -> restartQuiz() )
+                .setCancelable(false)
+                .show();
+
+
+    }
+
+    void restartQuiz(){
+        score = 0;
+        currentQuestionIndex =0;
+        loadNewQuestion();
+    }
+}
---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,23 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "Which is the capital of India ?",
+            "Which is the smallest prime number?",
+            "Whatsapp is a ...",
+            "Which is not a programming language?"
+    }   ;
+
+    public static String choices[][]={
+            {"Patna","Hyderabad","Kolkata","Delhi"},
+            {"3","2","5","1"},
+            {"hacking app","chat app","browser","music player"},
+            {"C","Anaconda","Python","Ruby"}
+    };
+    public static String correctAnswer[]={
+            "Delhi",
+            "2",
+            "chat app",
+            "Anaconda"
+    };
+}
